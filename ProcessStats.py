9b2e815f9f0db1906fdaf5b6c9473ebc683c4971
Blake Geno <blakegeno@gmail.com>
2015-04-07 19:55:29 -0400

    fixed pylint problems in ProcessStats.py

--- ProcessStats.py
+++ ProcessStats.py
@@ -1,39 +1,39 @@
 """
-
-
+takes a string that is a filename or directory and send the apropriate files
+to the csv_reader
 """
-import CSVStats
+import csv_stats
 import sys
 import os
 
 
-def isNCAAStatFile(filepath):
+def is_ncaa_stat_file(filepath):
+    """
+    check to see if the file is an ncaa stat file.
+    right now it just checks for the csv extension
+    """
     root, ext = os.path.splitext(filepath)
     return ext == '.csv'
-    
+
 
 def iterate_directory(path):
-
+    """run through all the entries in path and process the csv files"""
     for dirpath, dirnames, files in os.walk(path):
-        for f in files:
-            if(isNCAAStatFile(f)): 
-                CSVStats.CSVStatsReader(os.path.join(dirpath,f))
+        for filepath in files:
+            if is_ncaa_stat_file(filepath):
+                csv_stats.process_csv_file(os.path.join(dirpath, filepath))
 
 
-def process(fp):
-    if(os.path.isdir(fp)): iterate_directory(fp)
-    elif(isNCAAStatFile(fp)): 
-        CSVStats.CSVStatsReader(fp)
-    else: print("unrecognized file type") #TODO handle this more thoroughly   
+def process(filepath):
+    """main entry point for a string that is a filename or dirname"""
+    if os.path.isdir(filepath):
+        iterate_directory(filepath)
+    elif is_ncaa_stat_file(filepath):
+        csv_stats.process_csv_file(filepath)
+    else: print "unrecognized file type"
 
-
-
-def main(argv):
-    for fp in argv[1:]:
-        process(fp)
-        
 
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    process(sys.argv[1])
--- inputwindow.py
+++ inputwindow.py
@@ -1,64 +1,76 @@
+"""ui for inputting csv stat files"""
 import Tkinter as tk
 import ProcessStats
 import SettingsWindow
-import sys
-import os
+
 
 class InputWindow(tk.Frame):
+    """ui for inputting csv stat files"""
 
     def __init__(self, master=None):
-        self.createWindow(master)
-        self.populateObjects()
-
-    def createWindow(self, master):
         tk.Frame.__init__(self, master)
-        self.grid(ipadx=5,ipady=5)
+        #Attributes:
+        self.cancel_button = None
+        self.settings_button = None
+        self.entry_variable = None
+        self.text_entry = None
+        self.settings = None
+        self.submit_button = None
+        self._create_window(master)
+        self._populate_objects()
+
+
+    def _create_window(self, master):
+        """create the main window"""
+        self.grid(ipadx=5, ipady=5)
         self.master.title('Process Stat Files')
 
-    def populateObjects(self):
-        self.createTextEntry()
-        self.createSubmitButton()
-        self.createCancelButton()
-        self.createSettingsButton()
+    def _populate_objects(self):
+        """fill in the window"""
+        self._create_text_entry()
+        self._create_submit_button()
+        self._create_cancel_button()
+        self._create_settings_button()
 
-    def createSubmitButton(self):
-        self.submitButton = tk.Button(self, text='Submit', command=self.submitFile)
-        self.submitButton.grid(row=0, column=1)
+    def _create_submit_button(self):
+        """obvious"""
+        self.submit_button = tk.Button(self, text='Submit',
+                                      command=self.submit_file)
+        self.submit_button.grid(row=0, column=1)
+
+    def _create_cancel_button(self):
+        """obvious"""
+        self.cancel_button = tk.Button(self, text='Cancel', command=self.quit)
+        self.cancel_button.grid(row=0, column=2)
+
+    def _create_settings_button(self):
+        """obvious"""
+        self.settings_button = tk.Button(self, text='Settings',
+                                      command=self.show_settings)
+        self.settings_button.grid(row=1, column=1, padx=10, pady=10)
+
+    def _create_text_entry(self):
+        """obvious"""
+        self.entry_variable = tk.StringVar()
+        self.entry_variable.set('Enter a filename or directory')
+        self.text_entry = tk.Entry(self, exportselection=0, width=30,
+                                textvariable=self.entry_variable)
+        self.text_entry.grid(row=0, column=0, padx=10, pady=10)
+
+    def submit_file(self):
+        """called when the submit button is pressed - process the file"""
+        text = self.entry_variable.get()
+        if(text == 'Enter a filename or directory') | (text == ''):
+            pass
+        else:
+            ProcessStats.process(text)
+            self.entry_variable.set('Enter a filename or directory')
+
+    def show_settings(self):
+        """bring up the settings window"""
+        self.settings = SettingsWindow.SettingsWindow()
 
 
-    def createCancelButton(self):
-        self.cancelButton=tk.Button(self, text='Cancel', command=self.quit)
-        self.cancelButton.grid(row=0, column=2)
-
-
-    def createSettingsButton(self): #FIXME : change to menu option
-        self.settingsButton=tk.Button(self, text='Settings', command=self.showSettings)
-        self.settingsButton.grid(row=1, column=1, padx=10, pady=10)
-
-
-    def createTextEntry(self):
-        self.entryVariable = tk.StringVar()
-        self.entryVariable.set('Enter a filename or directory')
-        self.textEntry=tk.Entry(self, exportselection=0, width=30, 
-            textvariable=self.entryVariable)
-
-        self.textEntry.grid(row=0, column=0,padx=10, pady=10)
-
-
-
-    def submitFile(self):
-        f = self.entryVariable.get()
-        # don't take any action for no entry
-        if(f == 'Enter a filename or directory') | (f==''): pass
-        else: 
-            ProcessStats.process(f)
-            self.entryVariable.set('Enter a filename or directory')
-        
-    def showSettings(self):
-        self.settings=SettingsWindow.SettingsWindow()
-
-
-        
 if __name__ == '__main__':
     app = InputWindow()
     app.mainloop()
